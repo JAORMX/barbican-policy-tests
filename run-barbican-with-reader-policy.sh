737cf3c1cc423380fb04881a6b19eec00a619202
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2018-09-26 15:25:53 +0300

    Add policy file to replace the observer role with reader
    This is an exercise on changing the policy for barbican. So, this new
    policy file overwrites the default observer role and changes it for
    reader. Folks trying it out should notice that now using observer
    returns a 403.

---
+++ run-barbican-with-reader-policy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This runs a simple barbican container with host networking and as a daemon.
+# It takes into use policy-reader.json, which is a policy file that replaces
+# the "observer" role, for the "reader". This way, the operations that an
+# observer could usually do, are now doable by a reader. And using the observer
+# role should now give a 403.
+
+docker run -d --network=host \
+    -v $PWD/policy-reader.json:/etc/barbican/policy.json \
+    barbican-minimal:latest httpd -DFOREGROUND
