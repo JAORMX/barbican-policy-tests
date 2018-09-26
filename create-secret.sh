04d2dfaf3d5846e243d6dac81350b9497acada00
Juan Antonio Osorio Robles <jaosorior@redhat.com>
2018-09-26 15:56:07 +0300

    Print nicer json output in scripts.

--- create-secret.sh
+++ create-secret.sh
@@ -3,4 +3,4 @@ ROLE=$1
 curl -X POST -H 'X-Project-Id: 1234' -H 'Content-Type: application/json' \
     -H "X-Roles: $ROLE" \
     -d '{"name": "mySecrete", "payload": "mySecretPayload", "payload_content_type": "text/plain"}' \
-    http://localhost:9311/v1/secrets
+    http://localhost:9311/v1/secrets | python -m json.tool
--- list-secrets.sh
+++ list-secrets.sh
@@ -2,4 +2,4 @@
 ROLE=$1
 curl -X GET -H 'X-Project-Id: 1234' \
     -H "X-Roles: $ROLE" \
-    http://localhost:9311/v1/secrets
+    http://localhost:9311/v1/secrets | python -m json.tool
